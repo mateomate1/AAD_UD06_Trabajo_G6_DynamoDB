7723a9210dcd185878b615ddf58f22ce4c6840b7
Samuel Cobreros <cobrerossamuel@gmail.com>
2026-02-04 18:53:18 +0100

    aula_reserva

--- reserva_salas/src/main/java/g6/dynamodb/Model/Aula.java
+++ reserva_salas/src/main/java/g6/dynamodb/Model/Aula.java
@@ -2,4 +2,35 @@ package g6.dynamodb.Model;
 
 public class Aula {
 
+    private String id;
+    private Integer tamano;
+
+    public Aula() {}
+
+    public Aula(String id, Integer tamano) {
+        this.id = id;
+        this.tamano = tamano;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(Integer tamano) {
+        this.tamano = tamano;
+    }
+
+    @Override
+    public String toString() {
+        return "Aula [id=" + id + ", tamano=" + tamano + "]";
+    }
+
 }
--- reserva_salas/src/main/java/g6/dynamodb/Model/Reserva.java
+++ reserva_salas/src/main/java/g6/dynamodb/Model/Reserva.java
@@ -1,5 +1,70 @@
 package g6.dynamodb.Model;
 
+import java.time.LocalDateTime;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+@DynamoDBTable(tableName="reserva")
 public class Reserva {
+
+    private String id;
+    private LocalDateTime fechaInicio;
+    private LocalDateTime fechaFin;
+    private Integer nPersonas;
+    private Aula aula;
+    private Usuario usuario;
+
+    public Reserva() {}
+
+    public Reserva(String id, LocalDateTime fechaInicio, LocalDateTime fechaFin, Integer nPersonas, Aula aula, Usuario usuario) {
+        this.id = id;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.nPersonas = nPersonas;
+        this.aula = aula;
+        this.usuario = usuario;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public LocalDateTime getFechaInicio() {
+        return fechaInicio;
+    }
+    public void setFechaInicio(LocalDateTime fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+    public LocalDateTime getFechaFin() {
+        return fechaFin;
+    }
+    public void setFechaFin(LocalDateTime fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+    public Integer getnPersonas() {
+        return nPersonas;
+    }
+    public void setnPersonas(Integer nPersonas) {
+        this.nPersonas = nPersonas;
+    }
+    public Aula getAula() {
+        return aula;
+    }
+    public void setAula(Aula aula) {
+        this.aula = aula;
+    }
+    public Usuario getUsuario() {
+        return usuario;
+    }
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public String toString() {
+        return "Reserva [id=" + id + ", fechaInicio=" + fechaInicio + ", fechaFin=" + fechaFin + ", nPersonas=" + nPersonas + ", aula=" + aula + ", usuario=" + usuario + "]";
+    }
 
 }
