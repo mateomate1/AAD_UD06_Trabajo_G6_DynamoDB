8eeb20c68714f44e2ef93466fec078e4be68f640
Zacariadm <159574359+zacariadm@users.noreply.github.com>
2026-02-04 18:48:57 +0100

    minnor changes 0.1

--- reserva_salas/src/main/java/g6/dynamodb/Model/Aula.java
+++ reserva_salas/src/main/java/g6/dynamodb/Model/Aula.java
@@ -2,4 +2,5 @@ package g6.dynamodb.Model;
 
 public class Aula {
 
+        private String id;
 }
--- reserva_salas/src/main/java/g6/dynamodb/Model/Reserva.java
+++ reserva_salas/src/main/java/g6/dynamodb/Model/Reserva.java
@@ -2,4 +2,9 @@ package g6.dynamodb.Model;
 
 public class Reserva {
 
+    private String id;
+    private String aula;
+    private String fecha;
+    
+
 }
