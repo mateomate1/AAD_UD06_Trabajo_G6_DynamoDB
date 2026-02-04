573034048d3d98ba71cd484d0c22cc82f87a59c4
Zacariadm <159574359+zacariadm@users.noreply.github.com>
2026-02-04 23:33:54 +0100

    v0.0.1 rama para trabajar la interfaz

--- reserva_salas/src/main/java/g6/dynamodb/Main.java
+++ reserva_salas/src/main/java/g6/dynamodb/Main.java
@@ -1,31 +1,55 @@
 package g6.dynamodb;
 
+
+
 import g6.dynamodb.Model.Test;
 import g6.dynamodb.Util.AWSClient;
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
-        AWSClient aws;
-        try {
-            aws = new AWSClient(true);
-            aws.listTables().stream().forEach(System.out::println);
+        // AWSClient aws;
+        // try {
+        //     aws = new AWSClient(true);
+        //     aws.listTables().stream().forEach(System.out::println);
 
-            // Map<String, AttributeValue> item = new HashMap<>();
-            // item.put("id", new AttributeValue("USER3"));
-            // item.put("name", new AttributeValue("MARIO"));
+        //     // Map<String, AttributeValue> item = new HashMap<>();
+        //     // item.put("id", new AttributeValue("USER3"));
+        //     // item.put("name", new AttributeValue("MARIO"));
 
-            // // aws.insertItem(item);
-            // aws.scanTable("Usuarios").stream().forEach(System.out::println);
+        //     // // aws.insertItem(item);
+        //     // aws.scanTable("Usuarios").stream().forEach(System.out::println);
 
-            aws.generateTable(Test.class);
+        //     aws.generateTable(Test.class);
 
-            // aws.scanTable(Usuario.class).stream().forEach(System.out::println);
+        //     // aws.scanTable(Usuario.class).stream().forEach(System.out::println);
 
-            // aws.getItemById();
+        //     // aws.getItemById();
 
-        } catch (Exception e){
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        // } catch (Exception e){
+        //     // TODO Auto-generated catch block
+        //     e.printStackTrace();
+        // }
+
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        
+        // TODO: Implementar interfaz gráfica y manejarlo por el controlador
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ruta a archivo.fxml"));
+        Parent root = loader.load();
+        MainController controller = loader.getController();
+        Stage stage = new Stage();
+        stage.setTitle("Reserva de Salas");
+        stage.setScene(new Scene(root));
+        stage.show();
+        
+
     }
 }
---
+++ reserva_salas/src/main/java/g6/dynamodb/MainController.java
@@ -0,0 +1,20 @@
+package g6.dynamodb;
+import javafx.fxml.FXML;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class MainController {
+
+    @FXML
+    private Font x1;
+
+    @FXML
+    private Color x2;
+
+    @FXML
+    private Font x3;
+
+    @FXML
+    private Color x4;
+
+}
