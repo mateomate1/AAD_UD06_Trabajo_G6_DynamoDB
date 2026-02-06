667dc9eee13866aba0b92db6c0a2e5125bed98ff
Zacariadm <159574359+zacariadm@users.noreply.github.com>
2026-02-06 16:47:53 +0100

    minnor changes

--- reserva_salas/src/main/java/fx/App/ui/controladores/LoginController.java
+++ reserva_salas/src/main/java/fx/App/ui/controladores/LoginController.java
@@ -1,5 +1,29 @@
 package fx.App.ui.controladores;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class LoginController {
  // TODO: Implementar la lógica de autenticación y manejo de eventos para la pantalla de inicio de sesión.
+
+       @FXML
+    private Button ButtonLogIn;
+
+    @FXML
+    private Button ButtonSingUp;
+
+    @FXML
+    private TextField Usuario;
+
+    @FXML
+    void IniciarSesion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void RegistrarUsuario(ActionEvent event) {
+
+    }
 }
---
+++ reserva_salas/src/main/java/fx/App/ui/controladores/ReservaController.java
@@ -0,0 +1,40 @@
+package fx.App.ui.controladores;
+
+import g6.dynamodb.Style.Menu;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.web.WebView;
+
+public class ReservaController {
+    
+    // Conecta el FXML con el controller
+    
+    
+    
+    // Falta implementar la lógica de control para la pantalla de reservas, incluyendo la gestión de eventos y la interacción con el modelo de datos.
+    @FXML
+    private WebView WebViewPReservas;
+
+    @FXML
+    private Menu botonAuyudaPReservas;
+
+    @FXML
+    private Menu botonInicioPReservas;
+
+    @FXML
+    private Button botonReservaPReserva;
+
+    @FXML
+    private ComboBox<?> boxHorasPReservas;
+
+    @FXML
+    private DatePicker calendarioPReservas;
+
+    @FXML
+    private ListView<?> listAulasDisponibles;
+
+
+}
